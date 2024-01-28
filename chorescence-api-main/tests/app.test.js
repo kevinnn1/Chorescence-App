d7766617187b69e7f17c28f1574e83418139d730
kevinnn1 <47250733+kevinnn1@users.noreply.github.com>
2024-01-27 20:25:58 -0500

    removed api

--- chorescence-api-main/tests/app.test.js
+++
@@ -1,9 +0,0 @@
-const supertest = require("supertest");
-const app = require("../app");
-
-describe("Test the root path", () => {
-  test("GET /", async () => {
-    await supertest(app).get("/").
-      expect(400);
-  })
-});
